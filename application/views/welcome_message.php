f471e9d48e0f8823cce892def0bb708290f5f708
brantje <brantje@gmail.com>
2016-10-20 23:47:12 +0200

    Add viewport

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,7 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
 	<title>Welcome to Passman!</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="<?= base_url('resources/css/bootstrap.min.css'); ?>">
