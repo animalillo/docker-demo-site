abf44d042a5570d35652c63285514bba53764f9a
WolFi <wolfi@wolfi.es>
2017-01-02 18:38:21 +0100

    Added license and removed unused file

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Main page of the docker demo site.
+ * Copyright (C) 2016  Marcos Zuriaga Miguel <wolfi at wolfi.es>
+ * Copyright (C) 2016  Sander Brand <brantje at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
--- initd.sh
+++ initd.sh
@@ -9,6 +9,22 @@
 # Description:       Enable daemon functions (create docker instances, opening ports, ...)
 ### END INIT INFO
 
+# Daemon init script of the docker demo site
+# Copyright (C) 2016  Marcos Zuriaga Miguel <wolfi at wolfi.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 NAME=demo_site
 
 RUNDIR=/var/run
